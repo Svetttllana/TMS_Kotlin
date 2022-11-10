a0d5903afe2ee8bb148dfb3728705a2f8ad5bf9e
svetttllana <szgirovskaya@mail.ru>
2022-11-10 22:15:01 +0300

    Lesson 2; Kotlin; ACTIVITY: часть прилаги; и просто немножко проделжения урока 15

--- app/src/main/java/com/example/androidprogect_1/MainActivity.kt
+++ app/src/main/java/com/example/androidprogect_1/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.androidprogect_1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+val btnGoToActivity2 = findViewById<Button>(R.id.btnGoToActivity2)
+
+        btnGoToActivity2.setOnClickListener{
+            startActivity(
+                Intent(this,MainActivity2::class.java)
+                    .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK) // вот этим методом первая активити была удалена из бекстека.
+                    // теперь активити 2 явл корневой
+                        .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                //main activity2 will bot be added to the beckstack
+                 //   .addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
+
+
+            )
+        }
     }
 }
